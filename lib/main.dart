4edb1d6843123e3e5acd14fce4304303a96c6bb9
Krzysztof Rosiek <kmrosiek@gmail.com>
2024-05-15 12:47:17 +0200

    removed debug banner.

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Grocery app',
+      debugShowCheckedModeBanner: false,
       theme: AppTheme.bright(context),
       home: BlocProvider(
         create: (context) =>
